59a14100748ae491e6032c7d49011a6f9843b301
dextroxd <divyanshu171299@gmail.com>
2022-07-22 00:30:22 +0530

    Loader added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const navToggle = document.querySelector('.mobile-nav-toggle');
 const body = document.querySelector('body');
 const navMobile = document.querySelector('.mobile-nav');
 const linksInsideNav = document.querySelectorAll(".mobile-nav-items");
+const loader = document.querySelector('.lds-ripple');
 navToggle.addEventListener('click', () => {
     const visible = navToggle.getAttribute('data-visible');
     if (visible === 'false') {
@@ -26,3 +27,12 @@ linksInsideNav.forEach(item => {
         body.style.overflow = 'auto';
     });
 });
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        body.style.visibility = "hidden";
+        loader.style.visibility = "visible";
+    } else {
+        loader.style.display = "none";
+        body.style.visibility = "visible";
+    }
+};
